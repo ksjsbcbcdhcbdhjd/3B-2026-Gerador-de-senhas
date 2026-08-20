f324569af43db08d663cee2cd3778344ecdaa573
ksjsbcbcdhcbdhjd <maria.correa.portela@escola.pr.gov.br>
2026-08-20 12:39:20 -0300

    main.js

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
 const numeroSenha = document.querySelector(".parametro-senha__texto")
 numeroSenha.textContent = 5;
+
+tamanhoSenha = 8
+numeroSenha.textContent = tamanhoSenha;
+
+const botoes = document.querySelectorAll(".parametro-senha__botao")
+
+botoes [0].onclick = diminuir;
+
+function diminuir(){
+        tamanhoSenha--;
+        numeroSenha.textContent = tamanhoSenha;
+    
+}
+
+    
