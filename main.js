4ab2f74a5fe82babd6b9860c5d22570a05284f52
ksjsbcbcdhcbdhjd <maria.correa.portela@escola.pr.gov.br>
2026-08-27 12:39:53 -0300

    main.js

--- main.js
+++ main.js
@@ -1,17 +1,25 @@
 const numeroSenha = document.querySelector(".parametro-senha__texto")
-numeroSenha.textContent = 5;
+const campoSenha =  document.querySelector("#campo-senha")
+
+campoSenha.value = "Aqui vai aparecer a senha."
+
+let letrasMaiusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+//minusculas, numeros e simbolos
 
 tamanhoSenha = 8
 numeroSenha.textContent = tamanhoSenha;
 
 const botoes = document.querySelectorAll(".parametro-senha__botao")
-
+//pega o 1 botão = - 
 botoes [0].onclick = diminuir;
-
+// função diminuir 
 function diminuir(){
+        //diminui de 1 em 1 e costra na na tela 
         tamanhoSenha--;
         numeroSenha.textContent = tamanhoSenha;
     
 }
 
-    
+// BOTÃO DE AUMENTAR 
+
+ 
